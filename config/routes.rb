d5886fdb3bc7e3554b972558001a1272c994cf65
Ekrem Nurovic <ekrem.nurovic@gmail.com>
2019-08-10 10:34:05 +0100

    Thespoon rootes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #VERB path to: controller#action
+  root to: 'pages#home';
+  get 'restaurants', to: 'restaurants#index'
 end
