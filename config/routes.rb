aeb3abfd4820c2d681be873c94c7112814916334
Ekrem Nurovic <ekrem.nurovic@gmail.com>
2019-08-10 12:41:34 +0100

    The spoon

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  get 'restaurants', to: 'restaurants#index'
+  post 'restaurants', to: 'restaurants#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #VERB path to: controller#action
   root to: 'pages#home';
-  get 'restaurants', to: 'restaurants#index'
 end
